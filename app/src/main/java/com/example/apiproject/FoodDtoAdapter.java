f222aac68c22df37600e7d3c38a679fc785da3fc
Gokul41602 <gokul.18it@kct.ac.in>
2021-07-13 15:55:09 +0530

    Added favourites button functionality.

--- app/src/main/java/com/example/apiproject/FoodDtoAdapter.java
+++ app/src/main/java/com/example/apiproject/FoodDtoAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,18 +29,29 @@ public class FoodDtoAdapter extends RecyclerView.Adapter<FoodDtoAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.food_format,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.food_format, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull  FoodDtoAdapter.ViewHolder holder, int position) {
-        FoodDto foodDto=foodsList.get(position);
+    public void onBindViewHolder(@NonNull FoodDtoAdapter.ViewHolder holder, int position) {
+        FoodDto foodDto = foodsList.get(position);
         holder.foodTitle.setText(foodDto.getStrMeal());
         Picasso.with(context)
                 .load(foodDto.getStrMealThumb())
-                .resize(300,300)
+                .resize(300, 300)
                 .into(holder.foodImage);
+        holder.favourites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.favourites.setSelected(!holder.favourites.isSelected());
+                if (holder.favourites.isPressed()) {
+                    holder.favourites.setImageResource(R.drawable.ic_favourite);
+                } else {
+                    holder.favourites.setImageResource(R.drawable.ic_un_favourite);
+                }
+            }
+        });
 
     }
 
@@ -50,12 +62,15 @@ public class FoodDtoAdapter extends RecyclerView.Adapter<FoodDtoAdapter.ViewHold
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView foodTitle,foodDesc,foodCategory,foodArea;
+        private TextView foodTitle, foodDesc, foodCategory, foodArea;
         private ImageView foodImage;
+        private ImageButton favourites;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            foodTitle=itemView.findViewById(R.id.foodName);
-            foodImage=itemView.findViewById(R.id.foodImage);
+            foodTitle = itemView.findViewById(R.id.foodName);
+            foodImage = itemView.findViewById(R.id.foodImage);
+            favourites = itemView.findViewById(R.id.favouritesBtn);
         }
     }
 }
